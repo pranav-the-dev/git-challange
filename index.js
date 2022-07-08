eee3f5fc2c1bf53a35994de9d7ab672a53361d44
pranavdev540 <pranav.dev540@gmail.com>
2022-07-08 16:24:53 +0530

    working on port new

--- index.js
+++ index.js
@@ -5,7 +5,6 @@ var server = require('./lib/server')
 var port = process.env.PORT || 5000
 server().listen(port)
 console.log('second stage');
-console.log('working with second stage');
-console.log('listening on port ' + port);
+console.log('working with on port ' + port);
 console.log(name, 'listening on port', port)
 console.log('hello');
