a71e7670dae7f7d6391248e76191f9aee26327d9
pranavdev540 <pranav.dev540@gmail.com>
2022-07-08 16:19:51 +0530

    commit for adding data on index.js file

--- index.js
+++ index.js
@@ -5,5 +5,7 @@ var server = require('./lib/server')
 var port = process.env.PORT || 5000
 server().listen(port)
 console.log('second stage');
+console.log('working with second stage');
+console.log('listening on port ' + port);
 console.log(name, 'listening on port', port)
 console.log('hello');
