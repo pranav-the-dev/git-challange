a827fe763717563f3cb2db32a841c4b3fdce226f
pranavdev540 <pranav.dev540@gmail.com>
2022-07-08 16:27:02 +0530

    le leto ja

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-var name = require('./package.json').name
+var name = require('./package.jsoon').name
 require('productionize')(name)
 
-var server = require('./lib/server')
+var server = require('./lib/servers')
 var port = process.env.PORT || 5000
 server().listen(port)
-console.log('second stage');
+console.log('second working stage');
 console.log('working with on port ' + port);
 console.log(name, 'listening on port', port)
 console.log('hello');
