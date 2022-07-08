adeb82e0562bdf15fc2c0eeeeb96768f11ed6081
pranavdev540 <pranav.dev540@gmail.com>
2022-07-08 16:15:36 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,9 @@
+var name = require('./package.json').name
+require('productionize')(name)
+
+var server = require('./lib/server')
+var port = process.env.PORT || 5000
+server().listen(port)
+console.log('second stage');
+console.log(name, 'listening on port', port)
+console.log('hello');
